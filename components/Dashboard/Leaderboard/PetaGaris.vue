d86645d7f7a5db2077fc509091e5a7a4b925ee58
Dhias <dhiasmnaufal0@mail.ugm.ac.id>
2025-02-13 22:32:04 +0700

    add tod review

--- components/Dashboard/Leaderboard/PetaGaris.vue
+++ components/Dashboard/Leaderboard/PetaGaris.vue
@@ -9,9 +9,15 @@
       <template #subtitle>
         {{ leaderBoard[item.id] }}/{{ item.hasil }}
         <v-progress-linear
-          color="primary"
+          color="success"
+          buffer-color="info"
+          buffer-opacity="1"
           :model-value="(leaderBoard[item.id] / item.hasil) * 100"
           :height="10"
+          :buffer-value="
+            ((leaderBoard[item.id] + todoReview[item.id] || 0) / item.hasil) *
+            100
+          "
           rounded
         >
           <template v-slot:default="{ value }">
@@ -28,5 +34,8 @@ const employee: any = appMock.employee;
 const petaGarisStore = usePetaGarisStore();
 const leaderBoard: any = computed(() => {
   return petaGarisStore.leaderBoard;
+});
+const todoReview: any = computed(() => {
+  return petaGarisStore.todoReview;
 });
 </script>
--- stores/petaGaris.store.ts
+++ stores/petaGaris.store.ts
@@ -4,6 +4,7 @@ export const usePetaGarisStore = defineStore("petaGaris", () => {
   const totalGrid: any = ref(0);
   const totalArray: any = ref([]);
   const leaderBoard = ref({});
+  const todoReview = ref({});
   const queryAll = async () => {
     console.log("getBang");
     const db = useFirestore();
@@ -11,9 +12,16 @@ export const usePetaGarisStore = defineStore("petaGaris", () => {
       collectionGroup(db, "peta_garis_task"),
       where("status", "==", 3)
     );
+    const tasksQueryReview = query(
+      collectionGroup(db, "peta_garis_task"),
+      where("status", "==", 1)
+    );
     const tasksSnapshot = await getDocs(tasksQuery);
+    const toDoReview = await getDocs(tasksQueryReview);
     const taskCount: any = {};
+    const taskReviewCount: any = {};
     let results: any = [];
+    console.log(tasksQuery);
     tasksSnapshot.forEach((doc) => {
       const taskData = doc.data();
       const taskId = doc.id;
@@ -29,16 +37,34 @@ export const usePetaGarisStore = defineStore("petaGaris", () => {
         ...taskData,
       });
     });
+    toDoReview.forEach((doc) => {
+      // const taskData = doc.data();
+      // const taskId = doc.id;
+      const fullPath = doc.ref.path;
+      const employeeId = fullPath.split("/")[3];
+      if (!taskReviewCount[employeeId]) {
+        taskReviewCount[employeeId] = 0;
+      }
+      taskReviewCount[employeeId]++;
+      // results.push({
+      //   employeeId,
+      //   taskId,
+      //   ...taskData,
+      // });
+    });
     totalGrid.value = results.length;
     totalArray.value = results;
     leaderBoard.value = taskCount;
+    todoReview.value = taskReviewCount;
   };
+
   // queryAll();
   return {
     leaderBoard,
     totalGrid,
     totalArray,
     queryAll,
+    todoReview,
   };
 });
 
