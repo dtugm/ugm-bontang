dc7b23b4261483e647746700a19e9d1e7f4ef668
BEWILDERYAZ\dhias <dhiasmnaufal0@mail.ugm.ac.id>
2025-05-29 21:24:11 +0700

    Table basic Component

---
+++ components/app/table/Basic.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <div class="flex gap-3 items-center">
+      <AppTextH5 color="primary" class="mb-2">{{ title }}</AppTextH5>
+      <slot name="action"></slot>
+    </div>
+    <div class="flex justify-between items-center gap-2">
+      <slot name="prepend-table"></slot>
+      <slot name="validate-table"></slot>
+    </div>
+    <v-data-table class="header-theme mt-2" v-bind="$attrs">
+      <template #[`item.no`]="{ internalItem }">
+        <div>
+          {{ internalItem.index + 1 }}
+        </div>
+      </template>
+      <template
+        v-for="(_, name, slot_index) in $slots as {}"
+        :key="slot_index"
+        #[name]="scoped"
+      >
+        <slot :name="name" v-bind="{ ...(scoped as object) }"></slot>
+      </template>
+    </v-data-table>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  title: {
+    type: String,
+    default: "Table Data",
+  },
+});
+</script>
+<style scoped>
+.header-theme:deep() thead {
+  background-color: #6492b7;
+  color: white;
+}
+.header-theme:deep() .v-data-table-header__content span {
+  font-weight: bold;
+}
+</style>
