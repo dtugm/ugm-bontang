67f984ee7f364221b2748166e5125e9707c84f5a
DhiasNaufal <dhias@bangunindo.com>
2025-05-30 15:10:53 +0700

    Tambah api LotSurvey biar bagus namanya

---
+++ app/api/lotSurvey.api.ts
@@ -0,0 +1,35 @@
+import {
+  apiDeleteData,
+  apiGetData,
+  apiPostData,
+  apiPutData,
+  CustomFormData,
+} from "../helper";
+const mainPath = "/lot-survey-monitorings";
+
+export default {
+  getLotSurveyData: async (payload?: any) => {
+    return apiGetData(`${mainPath}?`, payload);
+  },
+  get_persil_by_fid: async (fid?: any) => {
+    return apiGetData(`${mainPath}/by-fid/${fid}`);
+  },
+  lot_survey_monitorings_summary: async (payload?: any) => {
+    return apiGetData(`${mainPath}/summary`, payload);
+  },
+  post_lot_survey_monitorings: async (payload: any) => {
+    return apiPostData(`${mainPath}`, payload, {
+      "Content-Type": "multipart/form-data",
+    });
+  },
+  put_lot_survey_monitorings: async (payload: any, id: string) => {
+    return apiPutData(`${mainPath}/${id}`, payload, {
+      "Content-Type": "multipart/form-data",
+    });
+  },
+  delete_lot_survey_monitorings: async (id: string) => {
+    return apiDeleteData(`${mainPath}/${id}`, {
+      "Content-Type": "multipart/form-data",
+    });
+  },
+};
