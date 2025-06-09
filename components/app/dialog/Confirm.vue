62a28b7bf96006f94014c1e1208064809577da10
DhiasNaufal <dhias@bangunindo.com>
2025-06-09 21:58:22 +0700

    Confirm Dialog

---
+++ components/app/dialog/Confirm.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-dialog
+    v-bind="$attrs"
+    :value="value"
+    width="500"
+    persistent
+    @input="input"
+  >
+    <v-card class="pa-4">
+      <v-card-title class="text-h5 grey lighten-2">
+        {{ title }}
+      </v-card-title>
+
+      <v-card-text class="py-4 px-4">
+        <template v-if="$slots.default">
+          <slot></slot>
+        </template>
+        <template v-else>
+          <div>This process cannot be reversed</div>
+          <div>Deleted data cannot be recovered</div>
+          <div>
+            If you want the same data, you have to re-enter the data that you
+            have deleted
+          </div>
+        </template>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-btn
+          variant="outlined"
+          class="flex-grow-1 text-none"
+          color="secondary"
+          @click="onClose"
+        >
+          {{ closeText }}
+        </v-btn>
+        <v-btn
+          class="flex-grow-1 text-none"
+          :color="confirmColor"
+          variant="flat"
+          :loading="confirmLoading"
+          @click="onConfirm"
+        >
+          {{ confirmText }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      default: "Are You Sure?",
+    },
+    confirmText: {
+      type: String,
+      default: "Yes, Delete",
+    },
+    confirmColor: {
+      type: String,
+      default: "error",
+    },
+    confirmLoading: {
+      type: Boolean,
+      default: false,
+    },
+    closeText: {
+      type: String,
+      default: "Not Now",
+    },
+  },
+  emits: ["input", "confirm", "close"],
+  methods: {
+    input(val) {
+      this.$emit("input", val);
+    },
+
+    onConfirm() {
+      this.$emit("confirm");
+    },
+
+    onClose() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
