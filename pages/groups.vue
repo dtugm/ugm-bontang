ad1463ef89ee71bdf1b2fb3397940b3c3dbe6fc9
DhiasNaufal <dhias@bangunindo.com>
2025-06-09 13:58:37 +0700

    Halaman Groups

---
+++ pages/groups.vue
@@ -0,0 +1,21 @@
+<template>
+  <AppTableBasic
+    title="Groups Table"
+    :items="groupStore.groupsItems"
+    :headers="groupListHeaders"
+  >
+    <template #item.member="{ item }">{{ item.members.length }}</template>
+  </AppTableBasic>
+</template>
+<script lang="ts" setup>
+const groupStore = useGroup();
+groupStore.getAllGroups();
+
+const groupListHeaders = [
+  { title: "No", key: "no" },
+  { title: "Group Name", key: "name" },
+  { title: "Description", key: "description" },
+  { title: "Member", key: "member" },
+  { title: "Actions", key: "action" },
+];
+</script>
