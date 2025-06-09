891daaeb3a76493a363e5eba2d3e5a66e8d865a9
DhiasNaufal <dhias@bangunindo.com>
2025-06-09 13:58:25 +0700

    Setup Group Store

--- stores/group.store.ts
+++ stores/group.store.ts
@@ -2,30 +2,15 @@ import groupApi from "~/app/api/group.api";
 import groupConstant from "~/app/constant/group.constant";
 import { defineStore } from "pinia";
 export const useGroup = defineStore("group", () => {
-  const lidarGroup = ref();
-  const getLidarGroup = async () => {
-    const groupId = groupConstant.groupId.lidar;
-
-    await groupApi.get_group_info_by_id(groupId).then((resp) => {
-      lidarGroup.value = resp;
-    });
+  const groupsItems = ref([]);
+  const getAllGroups = async () => {
+    const resp = await groupApi.get_all_groups();
+    groupsItems.value = resp;
   };
-  getLidarGroup();
-  const modelGroup = ref();
-  const getModelGroup = async () => {
-    const groupId = groupConstant.groupId.model;
-
-    await groupApi.get_group_info_by_id(groupId).then((resp) => {
-      modelGroup.value = resp;
-    });
-  };
-  getModelGroup();
   return {
-    getLidarGroup,
-    lidarGroup,
-
-    getModelGroup,
-    modelGroup,
+    // All Groups
+    getAllGroups,
+    groupsItems,
   };
 });
 
