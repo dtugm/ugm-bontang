01047aff4ae2df7e525dad0fb18527a708d21283
DhiasNaufal <dhias@bangunindo.com>
2025-06-09 13:57:57 +0700

    Auth Type

--- app/types/auth.d.ts
+++ app/types/auth.d.ts
@@ -3,3 +3,18 @@ declare interface ILoginPayload {
   password: string;
   username: string;
 }
+declare interface IUserInfoLogin {
+  email: string;
+  firstName: string;
+  groups: any[];
+  id: string;
+  lastName: string;
+  username: string;
+}
+declare interface IGroupInfo {
+  description: string;
+  name: string;
+  groups: any[];
+  id: string;
+  role: string;
+}
