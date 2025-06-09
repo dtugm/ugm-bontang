63383733b11e0786589c7ade27260eebc36db4b2
DhiasNaufal <dhias@bangunindo.com>
2025-06-09 13:54:27 +0700

    Register Group API

--- app/api/group.api.ts
+++ app/api/group.api.ts
@@ -8,10 +8,22 @@ import {
 const mainPath = "/groups";
 
 export default {
+  get_all_groups: async () => {
+    return apiGetData(`${mainPath}`);
+  },
   get_group_info_by_id: async (groupId: string) => {
     return apiGetData(`${mainPath}/${groupId}?`);
   },
   add_member_to_group: async (groupId: string, payload: any) => {
     return apiPostData(`${mainPath}/${groupId}/members`, payload);
+  },
+  delete_group: async (id: string) => {
+    return apiDeleteData(`${mainPath}/${id}`);
+  },
+  delete_member_on_group: async (id: string, memberId: string) => {
+    return apiDeleteData(`${mainPath}/${id}/members/${memberId}`);
+  },
+  edit_member_on_group: async (id: string, memberId: string, payload: any) => {
+    return apiPutData(`${mainPath}/${id}/members/${memberId}`, payload);
   },
 };
