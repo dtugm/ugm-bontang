75de76e6ca2aeed70c245dc49dba3aca0d206172
DhiasNaufal <dhias@bangunindo.com>
2025-06-09 13:47:06 +0700

    Make Placeholder Component page

---
+++ components/Placeholder/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row class="justify-center align-content-center text-center">
+        <v-col class="my-10 py-10">
+          <nuxt-link to="/" class="d-flex self-center">
+            <!-- <v-img src="/images/bakti.png" width="74px" height="32px"></v-img> -->
+          </nuxt-link>
+          <v-img
+            src="/illustration/placeholder.png"
+            class="mx-auto"
+            style="max-width: 450px"
+          >
+          </v-img>
+          <h1 class="text-h4 mb-2">This Page is under development</h1>
+          <p>
+            This page is currently undergoing scheduled development. We will
+            annouce you shortly
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
