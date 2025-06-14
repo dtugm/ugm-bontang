7bed249bbf73f816edeadf413cab73bc1c9c108f
DhiasNaufal <dhias@bangunindo.com>
2025-06-15 00:01:43 +0800

    Viewer Layout

---
+++ layouts/viewer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app" class="theme-default h-screen">
+    <slot />
+  </div>
+</template>
+<style scoped>
+.rotate-image {
+  transition: transform 0.3s ease-in-out;
+  transform-origin: center;
+}
+</style>
