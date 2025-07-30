f0a9d7445c879a001baaa2f37a5403b21b2a01ca
BEWILDERYAZ\dhias <dhiasmnaufal0@mail.ugm.ac.id>
2025-07-30 10:56:39 +0700

    Feat: Stable Version LotSuveyMonitoring API

---
+++ app/api/survey/lotSurveyMonitorings.api.ts
@@ -0,0 +1,45 @@
+import {
+  apiGetData,
+  apiUpdateWithStructuredPayload,
+  apiUploadWithStructuredPayload,
+  buildPath,
+  apiDeleteData,
+} from "~/app/axios/axiosBearer";
+
+const mainPath = "/lot-survey-monitorings";
+
+export default {
+  get_lot_survey_monitoring: async () => {
+    return apiGetData(`${mainPath}`);
+  },
+  create_lot_survey_monitoring: async (payload: any) => {
+    return apiUploadWithStructuredPayload(`${mainPath}`, payload);
+  },
+  get_summary_lot_survey_administrative: async (
+    path: IGetSummaryLotByAdministrative
+  ) => {
+    const customPath = buildPath(
+      `${mainPath}/summary/{administrativeLevel}`,
+      path
+    );
+    return apiGetData(customPath);
+  },
+  get_survey_by_fid: async (path: IGetSurveyByFid) => {
+    const customPath = buildPath(`${mainPath}/by-fid/{fid}`, path);
+    return apiGetData(customPath);
+  },
+  get_survey_by_id: async (path: IPathId) => {
+    const customPath = buildPath(`${mainPath}/{id}`, path);
+    return apiGetData(customPath);
+  },
+
+  update_lot_survey_monitoring: async (path: IPathId, payload: any) => {
+    const customPath = buildPath(`${mainPath}/{id}`, path);
+    return apiUpdateWithStructuredPayload(customPath, payload);
+  },
+
+  delete_lot_survey_monitoring: async (path: IPathId) => {
+    const customPath = buildPath(`${mainPath}/{id}`, path);
+    return apiDeleteData(customPath);
+  },
+};
