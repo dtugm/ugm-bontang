58268445515fea8c25939785ee31f7c244e8fcbf
BEWILDERYAZ\dhias <dhiasmnaufal0@mail.ugm.ac.id>
2025-07-30 10:56:26 +0700

    Feat: Stable Version Users Api

--- app/api/auth/users.api.ts
+++ app/api/auth/users.api.ts
@@ -1,9 +1,27 @@
-import { apiGetData } from "~/app/helper";
+import { apiGetData, apiPostData } from "~/app/axios/axiosBearer";
 
 const mainPath = "/users";
 
 export default {
+  register_user: async (payload: IRegisterUserPayload) => {
+    return apiPostData<IRegisterUserResponse>(`${mainPath}/register`, payload);
+  },
+  user_login: async (payload: IUserLoginPayload) => {
+    return apiPostData<IUserLoginResponse>(`${mainPath}/login`, payload);
+  },
+  refresh_token: async (payload: IRefreshTokenPayload) => {
+    return apiPostData<IRefreshTokenResponse>(
+      `${mainPath}/refresh-token`,
+      payload
+    );
+  },
+  edit_user: async () => {
+    return apiGetData(`${mainPath}`);
+  },
   get_all_users: async () => {
+    return apiGetData<IGetAllUserResponse>(`${mainPath}`);
+  },
+  get_current_user: async () => {
     return apiGetData(`${mainPath}`);
   },
 };
