c413ad5a367da304841e4e8fa51107f0eb11cbb7
DhiasNaufal <dhias@bangunindo.com>
2025-08-05 20:58:28 +0700

    Land Parcel Constant for Detail

---
+++ app/constant/view/landParcelViewer.constant.ts
@@ -0,0 +1,17 @@
+export default {
+  detailLandParcelHeader: [
+    { title: "Kelurahan", key: "village" },
+    { title: "NIB", key: "nib" },
+    { title: "Kepemilikan", key: "ownerType" },
+    { title: "STA WPOP", key: "staWpop" },
+    { title: "Alamat Object Pahak", key: "taxObjectAddress" },
+    { title: "RT", key: "rt" },
+    // { title: "", key: "taxObjectCode_1" },
+    // { title: "Tipe Lantai", key: "taxObjectCode_2" },
+    // { title: "Tipe Lantai", key: "taxPayerCode" },
+    { title: "Luas Bumi", key: "l_bumi" },
+    { title: "Luas Tertulis", key: "l_tertul" },
+    { title: "Pemakaian", key: "landUsage" },
+    { title: "Jenis tanah", key: "landType" },
+  ],
+};
