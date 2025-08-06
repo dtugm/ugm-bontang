5db761d42424829eb5439e8bc847a29c3b4952c0
DhiasNaufal <dhias@bangunindo.com>
2025-08-07 01:52:37 +0700

    Auth Helper

--- app/helper/auth.helper.ts
+++ app/helper/auth.helper.ts
@@ -2,3 +2,21 @@
 export function userInGroup(groups: any, groupName: String) {
   return groups.some((g: IGroupInfo) => g.name === groupName);
 }
+export function filterMenuByAccess(menu: any[], access: string[]): any {
+  return menu
+    .map((item) => {
+      if (item.path) {
+        return access.includes(item.path) ? item : null;
+      }
+
+      if (item.sub_menu) {
+        const filteredSub = filterMenuByAccess(item.sub_menu, access);
+        if (filteredSub.length > 0) {
+          return { ...item, sub_menu: filteredSub };
+        }
+      }
+
+      return null;
+    })
+    .filter(Boolean);
+}
