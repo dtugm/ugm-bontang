e3886f822a12961cbdeff54fe9a61b5670917895
DhiasNaufal <dhias@bangunindo.com>
2025-08-07 01:52:24 +0700

    User Management

---
+++ pages/admin/user_management.vue
@@ -0,0 +1,21 @@
+<template>
+  <AppTableBasic
+    :items="userStore.usersList"
+    :headers="usersListHeader"
+    title="List User"
+  />
+</template>
+
+<script setup lang="ts">
+const userStore = useUsersStore();
+userStore.getUsersList();
+const usersListHeader = [
+  { title: "No", key: "no" },
+  { title: "Email", key: "email" },
+  { title: "First Name", key: "firstName" },
+  { title: "Last Name", key: "lastName" },
+  { title: "ID Organisasi", key: "organizationId" },
+  { title: "Organisasi Name", key: "organizationName" },
+  { title: "Role", key: "role" },
+];
+</script>
