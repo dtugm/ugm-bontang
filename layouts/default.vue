649e89591178eae48a9dd64dc43fdd1b11638ceb
DhiasNaufal <dhias@bangunindo.com>
2025-08-08 16:03:07 +0700

    Brand New Layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,33 +2,104 @@
   <v-layout>
     <div class="relative">
       <v-navigation-drawer
+        flat
         v-model="drawer"
         :rail="rail"
         @click="rail = false"
-        class="bg-grayish"
+        class="bg-white border-none"
       >
         <div class="absolute w-full">
           <AppSidebar />
         </div>
-        <!-- <div class="absolute md:-right-3 -right-5 z-50 top-12">
-          <v-btn
-            color="grey"
-            :icon="rail ? 'mdi-chevron-right' : 'mdi-chevron-left'"
-            density="compact"
-            @click.stop="rail = !rail"
-          ></v-btn>
-        </div> -->
+
         <template v-slot:append>
           <AppButtonSignOut />
         </template>
       </v-navigation-drawer>
     </div>
-    <v-main class="bg-white">
+
+    <v-app-bar flat color="white" class="shadow-sm">
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title> {{ stringHelper.titleCase(pathName) }}</v-toolbar-title>
+      <v-spacer />
+      <!-- Nama pengguna -->
+      <v-menu :close-on-content-click="false" location="bottom">
+        <template v-slot:activator="{ props }">
+          <v-btn color="tertiary" v-bind="props" icon size="small">
+            <v-icon size="20">mdi-bell</v-icon>
+          </v-btn>
+        </template>
+        <v-card min-width="300">
+          <v-list>
+            <v-list-item
+              subtitle="You Have 3 Notification"
+              title="Notification"
+            >
+            </v-list-item>
+            <v-divider></v-divider>
+
+            <v-list-item
+              v-for="item in 3"
+              :key="item"
+              :value="item"
+              active-class="text-primary-blue"
+              class="py-3"
+            >
+              <v-list-item-title>{ item.title }</v-list-item-title>
+              <v-list-item-subtitle class="mb-1 text-high-emphasis opacity-100"
+                >{ item.headline }</v-list-item-subtitle
+              >
+
+              <v-list-item-subtitle class="text-high-emphasis"
+                >{ item.subtitle }</v-list-item-subtitle
+              >
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+      <v-menu :close-on-content-click="false" location="bottom left">
+        <template v-slot:activator="{ props }">
+          <v-btn color="tertiary" v-bind="props" icon size="small">
+            <v-icon size="32">mdi-account-circle</v-icon>
+          </v-btn>
+        </template>
+        <v-card min-width="200">
+          <v-list>
+            <v-list-item
+              :subtitle="authStore.user.email"
+              :title="authStore.user.username"
+            >
+              <template v-slot:prepend>
+                <v-avatar color="grey-lighten-1">
+                  <v-icon color="white">mdi-account</v-icon>
+                </v-avatar>
+              </template>
+            </v-list-item>
+            <v-divider></v-divider>
+
+            <v-card-text>
+              <AppButtonSignOut :is-button="true" />
+            </v-card-text>
+          </v-list>
+        </v-card>
+      </v-menu>
+    </v-app-bar>
+
+    <v-main class="bg-grayish">
       <slot />
     </v-main>
   </v-layout>
 </template>
+
 <script lang="ts" setup>
+import stringHelper from "~/app/helper/string.helper";
+
+const authStore = useAuthenticationStore();
+const route = useRoute();
 const drawer = ref(true);
 const rail = ref(false);
+const pathName = computed(() => {
+  const segments = route.path.split("/").filter(Boolean);
+  return segments[segments.length - 1] ?? "Dashboard";
+});
 </script>
