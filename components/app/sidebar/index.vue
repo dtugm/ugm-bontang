40b03b919aa39f8c06a374705f44e8a2b9b565ef
DhiasNaufal <dhias@bangunindo.com>
2025-08-08 16:03:01 +0700

    Brand New Side Bar

--- components/app/sidebar/index.vue
+++ components/app/sidebar/index.vue
@@ -1,80 +1,73 @@
 <template>
   <v-list>
     <v-list-item>
-      <v-img
-        :width="200"
-        aspect-ratio="16/9"
-        cover
-        src="/logo/logo_bontang_2.png"
-      ></v-img>
+      <template #prepend>
+        <v-img
+          src="https://digital-twin-ugm.s3.ap-southeast-1.amazonaws.com/assets/Logo_3.png"
+          width="40"
+          height="40"
+          class="rounded-0"
+          cover
+        />
+      </template>
+
+      <v-list-item-title>
+        <AppTextH3 class="px-4" color="tertiary">Bontang</AppTextH3>
+      </v-list-item-title>
     </v-list-item>
-    <!-- <v-list-item
-      prepend-icon="mdi-account"
-      :title="`${authStore.user.firstName} ${authStore.user.lastName}`"
-      :subtitle="authStore.user.username"
-    ></v-list-item> -->
   </v-list>
   <v-list mandatory density="compact" nav @update:selected="changePage">
-    <template v-for="(menu, index) in menus" :key="index">
+    <template v-for="(menu, index) in filteredMenus" :key="index">
       <v-list-group v-if="menu.sub_menu">
         <template v-slot:activator="{ props }">
           <v-list-item
             v-bind="props"
             :prepend-icon="menu.prepend_icon"
             :title="menu.title"
-          ></v-list-item>
+          >
+          </v-list-item>
         </template>
         <v-list-item
           v-for="(subMenu, index) in menu.sub_menu"
-          :key="index"
+          :key="subMenu.path"
           :active="subMenu.path === selectedTab"
           :prepend-icon="subMenu.prepend_icon"
           :title="subMenu.title"
           :value="subMenu.path"
-          active-class="bg-white"
-        ></v-list-item>
+          class="rounded-left"
+          active-color="tertiary"
+        >
+          <template v-slot:prepend>
+            <v-avatar>
+              <v-icon color="tertiary">{{ subMenu.prepend_icon }}</v-icon>
+            </v-avatar>
+          </template>
+        </v-list-item>
       </v-list-group>
       <v-list-item
         class="rounded-left"
         v-else
+        :ripple="false"
         :prepend-icon="menu.prepend_icon"
         :title="menu.title"
         :value="menu.path"
-        :active="menu.path === selectedTab"
-        active-class="bg-tertiary"
+        :active="
+          route.path === menu.path || route.path.startsWith(menu.path + '/')
+        "
+        active-color="tertiary"
       ></v-list-item>
     </template>
   </v-list>
 </template>
 <script lang="ts" setup>
-import appConstant from "~/app/constant/app.constant";
-import usersConstant from "~/app/constant/users.constant";
+import { filterMenuByAccess } from "~/app/helper/auth.helper";
+import userMenusConstant from "~/app/constant/user/userMenus.constant";
 const authStore = useAuthenticationStore();
+const filteredMenus = computed(() =>
+  filterMenuByAccess(userMenusConstant.All_Menus, authStore.userAccessPaths)
+);
 const route = useRoute();
 const selectedTab = ref(route.path);
-const arrays = [
-  usersConstant.pj_user,
-  usersConstant.stakeHolder_user,
-  usersConstant.superadmin_user,
-  usersConstant.leader_studio_users,
-  usersConstant.member_lidar_users,
-  usersConstant.member_3d_users,
-];
-const menuMap: any = {
-  0: appConstant.pj_menus,
-  1: appConstant.stakeHolder_menus,
-  2: appConstant.superadmin_menus,
-  3: appConstant.leader_studio_menus,
-  4: appConstant.member_lidar_users,
-  5: appConstant.member_3d_users,
-};
-const foundIndexes: any = arrays
-  .map((arr, index) => (arr.includes(authStore.user.email) ? index : -1))
-  .filter((index) => index !== -1);
-
-const menus: any = computed(() => {
-  return menuMap[foundIndexes];
-});
 const changePage = (item: any) => {
   selectedTab.value = item[0];
   navigateTo(item[0]);
