646b95d6ef39b03b529a4298226db9abf9a7f0e1
DhiasNaufal <dhias@bangunindo.com>
2025-08-19 13:55:10 +0700

    Feat: Bahasa indonesia

--- app/constant/user/userMenus.constant.ts
+++ app/constant/user/userMenus.constant.ts
@@ -26,26 +26,26 @@ export default {
     // Data Management
     {
       prepend_icon: "mdi-database-cog",
-      title: "Data Management",
+      title: "Manajemen Data",
       sub_menu: [
         {
           prepend_icon: "mdi-cube",
-          title: "3D Tiles",
+          title: "Data 3D",
           path: "/admin/model_3d_management",
         },
         {
           prepend_icon: "mdi-vector-polyline",
-          title: "2D Vector",
+          title: "Data Vector",
           path: "/admin/vector_2d_management",
         },
         {
           prepend_icon: "mdi-map",
-          title: "Land Parcel",
+          title: "Persil Tanah",
           path: "/data/land_parcel",
         },
         {
           prepend_icon: "mdi-office-building",
-          title: "Building",
+          title: "Bangunan",
           path: "/data/building",
         },
       ],
@@ -62,7 +62,7 @@ export default {
         },
         {
           prepend_icon: "mdi-cube-outline",
-          title: "3D Model",
+          title: "Model 3D",
           path: "/3d-model-monitoring",
         },
       ],
@@ -79,7 +79,7 @@ export default {
         },
         {
           prepend_icon: "mdi-map",
-          title: "Land Parcel",
+          title: "Persil Tanah 3D",
           path: "/land_parcel_cesium",
         },
       ],
