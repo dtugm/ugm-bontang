0894bc5632dabdab1f87a2da5b0936a2d037c274
DhiasNaufal <dhias@bangunindo.com>
2025-08-27 17:39:07 +0700

    Progress to suprvisor

--- components/Dashboard/Studio.vue
+++ components/Dashboard/Studio.vue
@@ -1,3 +1,33 @@
+<script setup lang="ts">
+// import { MonitoringSurveyPbbOptimized } from "#components";
+</script>
+
 <template>
-  <Placeholder />
+  <v-container fluid class="h-full">
+    <DashboardStakeHolderProgressKeseluruhan />
+    <v-row>
+      <v-col cols="12" sm="12">
+        <div class="h-[500px] rounded-md">
+          <MonitoringSurveyPbbOptimized />
+        </div>
+        <!-- <DashboardStakeHolderScurve /> -->
+      </v-col>
+    </v-row>
+    <DashboardStakeHolderProgressPbb />
+
+    <!-- <v-row>
+      <v-col cols="12" sm="12">
+        <v-card variant="flat">
+          <v-container class="h-full">
+            <MonitoringSurveyPbbLogbook />
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="12">
+        <DashboardStakeHolderScurve />
+      </v-col>
+    </v-row> -->
+  </v-container>
 </template>
