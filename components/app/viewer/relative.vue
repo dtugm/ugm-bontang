24a5752afc1eea2392058ced5f2b7c3d9e35e172
DhiasNaufal <dhias@bangunindo.com>
2025-09-15 05:38:07 +0700

    Global Component Menu and relative Container

---
+++ components/app/viewer/relative.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="isLoading" class="overlay-loading flex flex-col">
+    <v-progress-circular indeterminate color="white" size="64" />
+    <p class="text-white">Please wait...</p>
+  </div>
+  <div class="viewer-container relative w-screen h-screen">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  isLoading: {
+    type: Boolean,
+    required: false,
+  },
+});
+</script>
+<style scoped>
+.overlay-loading {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6); /* agak gelap */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+}
+.viewer-container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+</style>
